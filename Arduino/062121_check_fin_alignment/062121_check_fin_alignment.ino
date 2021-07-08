202be785dde460d1c964450d6b0327e5379be20a
James Chen <jmschen888@gmail.com>
2021-07-08 16:52:41 -0700

    added code to align fins

---
+++ Arduino/062121_check_fin_alignment/062121_check_fin_alignment.ino
@@ -0,0 +1,45 @@
+
+/*
+ * James Chen
+ * 06/21/21
+ * 
+ * Sets servos to common angle to adjust for discrepancies in alignment.
+ */
+
+#include <string.h>
+#include <SPI.h>
+#include <SD.h>
+#include <Servo.h>
+#include <inttypes.h>
+
+//Servos
+Servo U, D, L, R;
+int angle = 90;
+
+int servoRelay1 = 13;
+int servoRelay2 = 12;
+int servoRelay3 = 11;
+int servoRelay4 = 10;
+
+void setup() {
+  //attach servos to pins
+  R.attach(9);
+  L.attach(8);
+  U.attach(6);
+  D.attach(5);
+
+  digitalWrite(servoRelay1, LOW);
+  digitalWrite(servoRelay2, LOW);
+  digitalWrite(servoRelay3, LOW);
+  digitalWrite(servoRelay4, LOW);
+
+}
+
+void loop() {
+  //set servo angles, all share common angle
+  R.write(angle);
+  L.write(angle);
+  U.write(angle);
+  D.write(angle);
+
+}
